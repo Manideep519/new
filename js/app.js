3cd2e86d4025e829cb7fe0c5ea70ea665e7e3c65
manideep519 <manideepyadav359@gmail.com>
2021-09-29 01:13:07 +0530

    initial v.1.0

---
+++ js/app.js
@@ -0,0 +1,23 @@
+let boxOne = new hoverEffect({
+  parent: document.querySelector(".distortion--1"),
+  intensity: 0.5,
+  image1: "./../media/images/14.jpg",
+  image2: "./../media/images/13.jpg",
+  displacementImage: "./../media/images/disortion/7.jpg",
+});
+
+let boxTwo = new hoverEffect({
+  parent: document.querySelector(".distortion--2"),
+  intensity: 0.5,
+  image1: "./../media/images/10.jpg",
+  image2: "./../media/images/11.jpg",
+  displacementImage: "/../media/images/disortion/10.jpg",
+});
+
+let boxThree = new hoverEffect({
+  parent: document.querySelector(".distortion--3"),
+  intensity: 0.5,
+  image1: "./../media/images/8.jpg",
+  image2: "./../media/images/9.jpg",
+  displacementImage: "./../media/images/disortion/11.jpg",
+});
